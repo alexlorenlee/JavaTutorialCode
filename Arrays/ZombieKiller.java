5d213708e8886ac02e85cc919e94c2c5e0f49f48
FridomAraya21 <81179753+Fridom2020@users.noreply.github.com>
2021-05-25 18:06:07 -0700

    bought new weapon

--- Arrays/ZombieKiller.java
+++ Arrays/ZombieKiller.java
@@ -3,18 +3,20 @@ public class ZombieKiller {
 	public static void main(String[] args) {
 		System.out.println("Welcome to our zombie killer program!!!!");
 
-		String backpack[] = { "Shotgun", "Assault Rifle", "Sniper" };
-		String zombies[] = { "Close-range zombie", "Mid-range zombie", "Long-range zombie" };
+		String backpack[] = { "Machete","Shotgun", "Assault Rifle", "Sniper" };
+		String zombies[] = { "Hand to hand zombie","Close-range zombie", "Mid-range zombie", "Long-range zombie" };
 
 		System.out.println("Backpack items!");
 		System.out.println(backpack[0]);
 		System.out.println(backpack[1]);
 		System.out.println(backpack[2]);
+		System.out.println(backpack[3]);
 
 		System.out.println("These are the zombies!");
 		System.out.println(zombies[0]);
 		System.out.println(zombies[1]);
 		System.out.println(zombies[2]);
+		System.out.println(zombies[3]);
 	}
 
 }
